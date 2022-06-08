64f385c4f5d23426a968b973c5aff7f0052e8148
Warren Fletcher <warren@fletch.band>
2022-06-08 14:36:25 +0200

    Added VSCode config

--- get_configs.sh
+++ get_configs.sh
@@ -1,4 +1,5 @@
 cp /Users/$USER/.config/nvim/init.vim ./configs/
 cp ~/.tmux.conf.local ./configs/tmux.conf
 cp ~/.config/fish/config.fish ./configs/config.fish
+cp /Users/fletch/Library/Application\ Support/Code/User/settings.json ./configs/vscode/settings.json
 brew list > configs/brew_list
