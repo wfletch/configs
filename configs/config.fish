57d8a975e867d31e597a35d87ba2f5557b8bcade
Warren Fletcher <warren@fletch.band>
2023-02-18 00:00:00 +0100

    Auto Update: 02_18_2023

--- configs/config.fish
+++ configs/config.fish
@@ -9,21 +9,31 @@ end
 
 # Add Homebrew to Path
 fish_add_path -p /opt/homebrew/bin
-
+# Go Code
 alias gmi="go mod init"
+
+# Git Functions
 alias gi="git init"
+alias gs="git status"
 alias gA="git add *"
 alias ga="git add"
 alias gr="git pull --rebase"
 alias gp="git push"
 alias gc="git commit -m"
 alias e="exit"
+alias rstudio='/Applications/RStudio.app/Contents/MacOS/RStudio'
 alias gtkwave='/Applications/gtkwave.app/Contents/Resources/bin/gtkwave'
-
+fish_add_path "/opt/homebrew/anaconda3/bin/"
 function fish_user_key_bindings
   # ...
   bind \e\e 'thefuck-command-line'  # Bind EscEsc to thefuck
   # ...
 end
 fish_vi_key_bindings
+
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /opt/homebrew/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
 
